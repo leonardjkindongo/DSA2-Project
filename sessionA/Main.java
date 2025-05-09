f9501c772738fd8fa36f0bdc3479d45667bf6d5c
Jordan <indongoljk@protonmail.com>
2025-05-09 23:20:57 +0200

    Refined on 2025-05-09 at 11:20 PM

--- sessionA/Main.java
+++ sessionA/Main.java
@@ -1,34 +1,35 @@
-/**
- Main class to demonstrate the star topology network.
- Creates a network, adds nodes, and simulates messages.
- */
-
 public class Main {
     public static void main(String[] args) {
-        // Create the star network
-        StarTopology network = new StarTopology();
+        // Create a new star network
+        Star network = new Star();
 
-        // Add client nodes
-        network.insertNode("Client1");
-        network.insertNode("Client2");
-        network.insertNode("Client3");
+        // Add three client computers to the network
+        network.insertNode("Client1");  // Add first computer named "Client1"
+        network.insertNode("Client2");  // Add second computer named "Client2"
+        network.insertNode("Client3");  // Add third computer named "Client3"
 
-        // Get references to clients
-        ClientNode client1 = network.getClient("Client1");
-        ClientNode client2 = network.getClient("Client2");
-        ClientNode client3 = network.getClient("Client3");
+        // Get handles to each client so we can send messages later
+        ClientNode client1 = network.getClient("Client1");  // Get reference to Client1
+        ClientNode client2 = network.getClient("Client2");  // Get reference to Client2
+        ClientNode client3 = network.getClient("Client3");  // Get reference to Client3
 
         // Send messages between clients
         if (client1 != null && client2 != null) {
+
+            // Client1 sends "Hello" to Client2
             client1.send("Client2", "Hello from Client1!");
+            
+            // Client2 replies to Client1
             client2.send("Client1", "Hi there, Client1!");
         }
 
-        // Demonstrate removing a node
+        // Remove Client3 from the network
         network.deleteNode("Client3");
 
-        // Try sending to removed client
+        // Try sending a message to the removed client (should fail)
         if (client1 != null) {
+
+            // This message won't be delivered because Client3 is gone
             client1.send("Client3", "This should fail");
         }
     }
